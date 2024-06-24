5913a7a95cc12e59f75f6a20d9ce08e022d2c157
lakdjs <124062365+lakdjs@users.noreply.github.com>
2024-06-24 11:03:25 +0300

    finished last kt

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //last kt
+            Console.WriteLine("напишите строку");
+            string a =  Console.ReadLine();
+            
+
+
+            int Determine(string s)
+            {
+                int left = 0;
+                int right = 0;
+                int max = 0;
+
+                
+                var chars = s.ToCharArray();
+                string sub = "";
+
+                while (right < chars.Length)
+                {
+                    if (sub.Contains(chars[right]))
+                    {
+                        sub = "";
+                        left++;
+                    }
+                    else
+                    {
+                        sub += chars[right];
+                        right++;
+                        max = Math.Max(max, sub.Length);
+                    }
+                }
+
+                return max;
+            }
+
+            int maxlen = Determine(a);
+            Console.WriteLine(maxlen);
+        }
+    }
+}
